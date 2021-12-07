a99e37b1688f4e8076bd86fc4ee670cd39dc0c07
peppergv4 <59515019+peppergv4@users.noreply.github.com>
2021-12-06 17:53:59 -0800

    removed class names
    Set classes to blank and started custom css rewrite

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ function Home() {
    const [loading, setLoading] = useState(false)
    const [infoIndex, setInfoIndex] = useState(0)
    const [turn, setTurn] = useState(0)
-   const [imgIndex, setImg] = useState(0) 
+   const [imgIndex, setImg] = useState(0)
    const imgCount = 5;
 
    useEffect(() => {
@@ -34,7 +34,7 @@ function Home() {
    }, [])
 
 
-   
+
    // Button functionality for image slider
    function btnLeft() {
       if (imgIndex == 0) {
@@ -46,7 +46,7 @@ function Home() {
             return cur - 1;
          })
       }
-   } 
+   }
 
    function btnRight() {
       if (imgIndex == imgCount - 1) {
@@ -63,17 +63,17 @@ function Home() {
    return (
 
       <main>
-         <section className='home-section flex w-screen rel-height h-1/4 md:h-1/2'>
-            <div className='bg-gray-500 flex-1 w-1/2'>
-               <div className="relative grad-bkg h-full w-full">
-                  <div className="flex invisible md:visible logo absolute ml-5 mt-5 w-32 h-14 bg-blue-dark shadow-logo blur-sm hover:bg-dark-blurple">
-                     <h1 className='flex mx-auto items-center h-full leading-5 font-lato font-light text-center text-2xl tracking-wide text-semi-white'>YT Diff</h1>
+         <section className='topHalfCont'>
+            <div className='topLeftCont'>
+               <div className="tlcBkg gradBkg">
+                  <div className="logoCont">
+                     <h1 className='logoText'>YT Diff</h1>
                   </div>
-                  <div className="align-self w-auto relative top-4 md:top-28 ">
-                     <h1 className="text-center sm:drop-shadow-lg text-2xl md:text-3xl lg:text-4xl font-lato font-light tracking-wide text-semi-white">A tool for comparing video playlists on YouTube.</h1>
+                  <div className="blank">
+                     <h1 className="blank">A tool for comparing video playlists on YouTube.</h1>
                   </div>
-                  <div className="absolute w-9/12 bottom-8 md:bottom-28 right-2 md:right-8">
-                     <div className="h-full w-full">
+                  <div className="blank">
+                     <div className="blank">
                         {
                            // On every even turn, the first one will come in and the second one will stay.
                            // The new one coming in will have the new value, and the one going out will have the last value.
@@ -90,8 +90,8 @@ function Home() {
                         }
                      </div>
                   </div>
-                  <div className="absolute w-9/12 bottom-8 md:bottom-28 right-2 md:right-8">
-                     <div className="h-full w-full">
+                  <div className="blank">
+                     <div className="blank">
                         {
                            // If it is it's turn to come in, get current element.
                            ((turn) % 2 == 1) ? <InfoText text={infoData[infoIndex]['info']} comeIn={true}></InfoText> :
@@ -105,27 +105,27 @@ function Home() {
                   </div>
                </div>
             </div>
-            <div className="relative flex-1 w-1/2">
-               <div className="ui-element-cont w-1/2 h-1/2">
+            <div className="blank">
+               <div className="blank">
 
-                  <div className="absolute img-cont w-full h-full">
+                  <div className="blank">
                      {
                         loading ? <img src="https://fakeimg.pl/720x400/" alt="placeholder" className='fit-img' /> : <img src="/images/img1.jpeg" alt="main img" className='fit-img' />
                      }
                   </div>
 
 
-                  <div className="absolute z-10 capt-bar h-16 md:h-14 bg-captBar shadow-captBar flex items-center w-full">
-                     <div className="caption w-full flex items-center justify-center">
-                        <h1 className="text-blue-dark font-light text-2xl text-center">Here is the caption</h1>
+                  <div className="blank">
+                     <div className="blank">
+                        <h1 className="blank">Here is the caption</h1>
                      </div>
                   </div>
 
-                  <div className="absolute arrow-cont z-20 flex justify-center items-center shadow-arrowBox bg-arrowBox  h-5 rounded-xl w-14">
-                     <button onClick={btnLeft} className="left-btn w-1/2">
+                  <div className="blank">
+                     <button onClick={btnLeft} className="blank">
                         <FontAwesomeIcon icon={faArrowLeft} />
                      </button>
-                     <button onClick={btnRight} className="right-btn w-1/2">
+                     <button onClick={btnRight} className="blank">
                         <FontAwesomeIcon icon={faArrowRight} />
                      </button>
                   </div>
